aaf98e02a18b55dd55ac07ae437d5ca806899bdc
Gabriel <GabrielSilvadearaujo142@gmail.com  git config --global user.name Gabrielgit config --global user.email GabrielSilvadearaujo142@gmail.com>
2026-04-24 11:38:39 -0300

    API que busca a informacao do tempo

--- Index.js
+++ Index.js
@@ -1,28 +1,26 @@
 // Configurando o server!
 const Express = require("express");
+const axios = require("axios")
+const API_KEY = '101f4a830be695cb0146630eff0489a3';
 const app = Express();
-const {getWeatherInfo, getAllWeatherInfo} = require("./cityDb.js");
 app.use(Express.json())
 app.listen(5000, ()=>console.log("server running"));
 
 
-//Rota que buscar as informações sobre o tempo de uma cidade especifica em um banco de dados externo!
+//Rota que buscar as informações sobre o clima de uma cidade especifica!
 
 app.get("/getWeather/:city", async (req, res)=>{
+    const cityParam = req.params.city;
     try{
-    const result = await getWeatherInfo(req.params.city);
-    res.json(result);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityParam}&appid=${API_KEY}&units=metric`);
+        const dados = response.data;
+        const result = {
+            "cidade": dados.name,
+            "temperatura": dados.main.temp,
+            "descrição": data.weather[0].description
+        }
+        res.json(result);
     } catch(error){
         res.status(500).json({ERR: "erro ao buscar dado", motivo: error
         });
-    }
-})
-
-app.get("/getAllWeather", async (req, res)=>{
-    try{
-    const result = await getAllWeatherInfo();
-    res.json(result);
-    } catch(error){
-        res.status(500).json({ERR: "erro ao buscar dado", motivo: error})
-    }
-})
+    }})

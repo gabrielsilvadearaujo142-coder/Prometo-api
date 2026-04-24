7ce4813cafbf5744d4fb2bee30e5bae51d5ee84d
Gabriel <GabrielSilvadearaujo142@gmail.com  git config --global user.name Gabrielgit config --global user.email GabrielSilvadearaujo142@gmail.com>
2026-04-24 14:35:15 -0300

    api com todas as chaves credenciais ocultas

--- Index.js
+++ Index.js
@@ -1,23 +1,24 @@
 // Configurando o server!
 const Express = require("express");
 const axios = require("axios")
-const API_KEY = '101f4a830be695cb0146630eff0489a3';
+const API_KEY = process.env.APIKEY;
 const app = Express();
 app.use(Express.json())
-app.listen(5000, ()=>console.log("server running"));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>console.log("server running"));
 
 
 //Rota que buscar as informações sobre o clima de uma cidade especifica!
 
 app.get("/getWeather/:city", async (req, res)=>{
-    const cityParam = req.params.city;
+    const city = req.params.city;
     try{
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityParam}&appid=${API_KEY}&units=metric`);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
         const dados = response.data;
         const result = {
             "cidade": dados.name,
             "temperatura": dados.main.temp,
-            "descrição": data.weather[0].description
+            "descrição": dados.weather[0].description
         }
         res.json(result);
     } catch(error){

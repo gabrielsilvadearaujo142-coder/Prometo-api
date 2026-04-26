17ee26989cce67e197c6c74bb48439f00760abbd
gabrielsilvadearaujo142-coder <gabrielsilvadearaujo142@gmail.com>
2026-04-26 17:19:11 -0300

    Update database connection parameters for pool

--- voluntariosDb.js
+++ voluntariosDb.js
@@ -1,11 +1,11 @@
 
 import  {Pool} from "pg";
 const pool = new Pool({
-    host : process.env.HOST,
-     user : process.env.USER,
-    database : process.env.DATABASE,
-    port : process.env.PORT,
-    password : process.env.PASSWORD
+    host : process.env.HOST_db
+     user : process.env.USER_db,
+    database : process.env.DATABASE_db,
+    port : process.env.PORT_db,
+    password : process.env.PASSWORD_db
 });
 
 

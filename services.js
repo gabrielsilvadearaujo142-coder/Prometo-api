04a858da6303d712c3446a7e85688a40c7b74f5c
Gabriel <GabrielSilvadearaujo142@gmail.com  git config --global user.name Gabrielgit config --global user.email GabrielSilvadearaujo142@gmail.com>
2026-04-27 02:00:48 -0300

    services.js fix

--- services.js
+++ services.js
@@ -17,11 +17,11 @@ async function estrategiafunc(grupos){
         }
         estrategia.push({
             'grupo': grupo,
-            'lider': lider.nome || "vaga aberta para lider",
-            'suporte': suporte.nome || "vaga aberta para suporte",
-            'backup': backup.nome || "vaga aberta para backup",
+            'lider': lider?.nome || "vaga aberta para lider",
+            'suporte': suporte?.nome || "vaga aberta para suporte",
+            'backup': backup?.nome || "vaga aberta para backup",
             "restante da equipe" : equipe.map(p => p.nome),
-            "plano" : ` O lider do time  coorderna com ajuda do suporte e  backup`
+            "plano" : ` O lider do time  coordena com ajuda do suporte e  backup`
         })
     }
     return estrategia;
